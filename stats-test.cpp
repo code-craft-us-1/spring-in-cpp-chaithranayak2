4915ec10805490d91455061c6712b3ce64568210
Chaithra Nayak <119813807+chaithranayak2@users.noreply.github.com>
2024-08-26 07:16:25 +0000

    Appr1

--- stats-test.cpp
+++ stats-test.cpp
@@ -13,9 +13,6 @@ TEST(Statistics, ReportsAverageMinMax) {
 
 TEST(Statistics, AverageNaNForEmpty) {
     auto computedStats = Statistics::ComputeStatistics({});
-    //All fields of computedStats (average, max, min) must be
-    //NAN (not-a-number), as defined in math.h
-    
-    //Design the gtest EXPECT statement here.
-    //Use http://www.cplusplus.com/reference/cmath/isnan/
+
+    EXPECT_EQ(std::isnan(computedStats.average));
 }
--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,24 @@
 #include "stats.h"
+#include <cmath>
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+using namespace Statistics;
+
+Stats Statistics::ComputeStatistics(const std::vector<float>& input) {
     //Implement statistics here
+    Stats statsanswer;
+    statsanswer.average = NAN;
+    statsanswer.max = NAN;
+    statsanswer.min = NAN;
+    float sum = 0;
+
+    for(auto it : input){
+        sum += it;
+        statsanswer.max = (statsanswer.max > it) ? statsanswer.max : it ;
+        statsanswer.min = (statsanswer.min < it) ? statsanswer.min : it ;
+    }
+
+    statsanswer.average = sum/input.size();
+
+    return statsanswer;
+
 }
